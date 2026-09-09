128ef326d50baf2ec456c371d0f17f14c3b5a88e
Alex Riepl Broz <riepla@gmail.com>
2026-09-09 11:10:38 +0800

    Update app.js
    added carto api key

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const map = L.map("map", {
 });
 
 L.tileLayer(
-  "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
+  "https://basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png?key=cb1_316i_1_7420184ded0f7b53c5cc16cd",
   {
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a>, &copy; CARTO'
